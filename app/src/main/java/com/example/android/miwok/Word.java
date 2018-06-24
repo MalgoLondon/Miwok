6eb2fa22a165286b65175c045b42a367d0e59c7e
Malgo <malgo.babiarz.wardrobe@gmail.com>
2018-06-24 21:46:27 +0100

    next version of Miwok App

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -9,14 +9,38 @@ package com.example.android.miwok;
         //**we need 2 fields for the state of the word object:*/
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    /**
+     * Constant value that represents no image was provided for this word
+     */
+    private static final int NO_IMAGE_PROVIDED = -1;
+    /**
+     * Audio resource ID for the word
+     */
+    private int mAudioResourceId;
+    /**
+     * Image resource ID for the word
+     */
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
 
-    //**then we need to create a constructor which takes in two parameters: mDefaultTranslaton, mMiwokTranslation
+    //**then we need to create a constructor (create an object) which takes in 4 parameters: mDefaultTranslaton, mMiwokTranslation, mAudioResourceId
     // The name of the constructor needs to match exactly the name of the class
     // it needs to be public (access variable) so outside classes can access it
     // this is a setter method*/
-    public Word (String defaultTranslation, String miwokTranslation){
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
         mDefaultTranslation=defaultTranslation;
-        mMiwokTranslation=miwokTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
+    }
+
+    //**then we need to create a constructor (create an object) which takes in 4 parameters: mDefaultTranslaton, mMiwokTranslation, mImageResourceId, mAudioResourceId
+    // The name of the constructor needs to match exactly the name of the class
+    // it needs to be public (access variable) so outside classes can access it
+    // this is a setter method*/
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
     }
 
     //**adding 2 methods with purpose to return the translations
@@ -24,7 +48,23 @@ package com.example.android.miwok;
     public String getDefaultTranslation() {
         return mDefaultTranslation;
     }
+
     public String getMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    /**
+     * Returns whether or not there is an image for this word.
+     */
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+    public int getAudioResourceId() {
+        return mAudioResourceId;
     }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.miwok;
 
 import android.app.Activity;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,12 +16,16 @@ import java.util.ArrayList;
  */
 
 public class WordAdapter extends ArrayAdapter<Word> {
-    public WordAdapter(Activity context, ArrayList<Word> numbers) {
+
+    private int mColorResourceId;
+
+    public WordAdapter(Activity context, ArrayList<Word> numbers, int colorResourceId) {
         // **Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
         // We use 0 as a second argument because we don't need to rely on the ArrayAdapter to create a ListView for us.
         // Instead, the getView method will handle the inflating of the layout from the layout ressource ID.*/
         super(context, 0, numbers);
+        mColorResourceId = colorResourceId;
     }
     @Override
     //**Provides a view for an adapter View (ListView, GridView)
@@ -50,6 +55,25 @@ public class WordAdapter extends ArrayAdapter<Word> {
         // Get the English word from the current Word object and
         // set this text on the number TextView
         defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        // Find the ImageView in the list_item.xml layout with the ID list_icon.
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.list_icon);
+        // Check if an image is provided for this word or not
+        if (currentWord.hasImage()) {
+            // If an image is available, display the provided image based on the resource ID
+            imageView.setImageResource(currentWord.getImageResourceId());
+            // Make sure the view is visible
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            // Otherwise hide the ImageView (set visibility to GONE)
+            imageView.setVisibility(View.GONE);
+        }
+        // Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        // Set the background color of the text container View
+        textContainer.setBackgroundColor(color);
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
